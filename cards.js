de439dbbaacdbb48470a57b46b20d90449b0a7a1
murffious <titan.murff@gmail.com>
2019-08-24 12:53:20 -0600

    first commit

---
+++ cards.js
@@ -0,0 +1,28 @@
+// make a network request to "shuffle" or generate a deck of cards.
+fetch('https://deckofcardsapi.com/api/deck/new/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  });
+
+// With the returned deck id, make subsequent network 
+//requests drawing 2 cards at a time.
+
+// Keep drawing until each suit's Queen card is drawn. Then stop. 
+
+
+// Provide separate lists of all cards drawn for each suit.
+// At most make one network request each second.
+
+
+// Your final solution should print to the screen the 
+// array of drawn cards for each suit. 
+
+
+// Each array should be sorted. For example, your code should print something like:
+// SPADES: [2, 3, 5, 10, JACK, QUEEN]
+// CLUBS: [ACE, 2, 4, 5, 6, 10, JACK, QUEEN, KING]
+// HEARTS: [2, 3, 5, JACK, QUEEN, KING]
+// DIAMONDS: [ACE, 2, 3, 5, 6, 7, 8, 10, QUEEN]
